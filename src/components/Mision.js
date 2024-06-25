11571b56576fc18d42f3e61f81b99030c39a3300
Gabriel Aizman <gabaiz88@gmail.com>
2024-06-25 15:17:49 +0200

    version 2.0

--- src/components/Mision.js
+++ src/components/Mision.js
@@ -16,7 +16,6 @@ const Mision = () => {
             <div className='bg-orange_card p-5 sm:w-2/4 flex flex-col justify-start sm:p-10'>
                 <div className=''>
                     <h2 className="text-5xl mb-1 text-black">Misión</h2>
-                    <img src={linea} className='w-32 mr-3' alt="linea"></img>
                 </div>
                 <div className='w-full sm:w-9/12 text-lg text-black mt-10'>
                     <p>Nuestra misión es proporcionar envases descartables y artículos de cotillón de alta calidad que añadan valor a las celebraciones y la vida cotidiana de nuestros clientes. Nos dedicamos a innovar y ofrecer productos que cumplan con las expectativas de funcionalidad y diseño, asegurando siempre la satisfacción del cliente.</p>
