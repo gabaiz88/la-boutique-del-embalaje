f5b8be5803d43f65b137fca548d64eadcf9fdf26
Gabriel Aizman <gabaiz88@gmail.com>
2024-06-25 14:35:14 +0200

    version 2.0

--- src/components/WhatsappIcon.js
+++ src/components/WhatsappIcon.js
@@ -5,7 +5,7 @@ import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 const WhatsappIcon = () => {
   return (
     <a id="whatsapp"
-      href="https://wa.me/+5491168813544" // Reemplaza con tu número de WhatsApp
+      href="https://wa.me/+5491168813544"
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-16 sm:right-10 bg-green-500 text-white p-4 rounded-full shadow-lg hover:bg-green-600 transition duration-300"
