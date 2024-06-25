088fc6a22c4d4975d018f548cf2095492ffa6d3d
Gabriel Aizman <gabaiz88@gmail.com>
2024-06-25 14:41:13 +0200

    version 2.0

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import logo from '../img/logo.png';
 const Footer = () => {
   return (
     <footer class="bg-white dark:bg-gray-900">
-      <div class="container flex flex-col items-center justify-between p-6 mx-auto space-y-4">
+      <div class="flex flex-col items-center justify-between p-6 mx-auto space-y-4">
           <img class="w-32 h-auto" src={logo} alt="" />
         <p class="text-sm text-gray-600 dark:text-gray-300">© Copyright 2021. All Rights Reserved.</p>
 
