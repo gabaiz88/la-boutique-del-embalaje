f65bc457a4204a5a3dd4dea3d6f252e7296d9289
Gabriel Aizman <gabaiz88@gmail.com>
2024-06-25 14:50:27 +0200

    version 2.0

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ import linea from '../img/linea_crayon.jpg'
 
 const About = () => {
   return (
-    <div id="about" className="flex flex-col w-full gap-10 p-5 sm:p-0 sm:flex-row justify-center sm:w-10/12 sm:mx-auto sm:my-10 sm:gap-32">
+    <div id="about" className="flex flex-col gap-10 p-5 sm:p-0 sm:flex-row justify-center sm:w-10/12 sm:mx-auto sm:my-10 sm:gap-32">
       <div className='flex flex-col w-full sm:w-2/4 justify-between gap-10 sm:gap-0'>
         <div className=''>
           <h2 className="text-5xl mb-2">Acerca de Nosotros</h2>
