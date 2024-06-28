0103423e642796cb27e3180ddab5439af676dbd8
Gabriel Aizman <gabaiz88@gmail.com>
2024-06-28 20:25:49 +0200

    version 2.0

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Carousel from './components/Carousel';
